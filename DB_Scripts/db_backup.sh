1155d3a42b11bfd5aca1ed9770a5507e03d8c513
Evan Jelley <jelleyevan@gmail.com>
2024-08-02 16:00:50 -0600

    fix deploy

--- DB_Scripts/db_backup.sh
+++
@@ -1,5 +0,0 @@
-DATABASE_PATH=/schoolsAPI/db.sqlite3
-BUCKET_NAME=cfb-realignment-site
-REGION_NAME=us-east-2
-
-aws s3 cp $DATABASE_PATH s3://$BUCKET_NAME/database/db.sqlite3 --region $REGION_NAME
--- DB_Scripts/db_restore.sh
+++
@@ -1,5 +0,0 @@
-DATABASE_PATH=/schoolsAPI/db.sqlite3
-BUCKET_NAME=cfb-realignment-site
-REGION_NAME=us-east-2
-
-aws s3 cp s3://$BUCKET_NAME/database/db.sqlite3 $DATABASE_PATH --region $REGION_NAME

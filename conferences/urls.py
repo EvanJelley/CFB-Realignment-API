efdeaa9b1a432fdec0c7dce9a151c3abda5b13d6
Evan Jelley <jelleyevan@gmail.com>
2024-08-06 14:49:20 -0600

    Update urls.py

--- conferences/urls.py
+++ conferences/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('conferencebyyear/', AllConferenceByYearList.as_view()),
     path('conferencebyyear/<int:pk>/', ConferenceByYearDetail.as_view()),
     path('conferencelogos/', ConferenceWithLogoList.as_view()),
-    path('schoollogos/', SchoolWithLogoList.as_view())
+    path('schools/', SchoolWithLogoList.as_view())
 ]
